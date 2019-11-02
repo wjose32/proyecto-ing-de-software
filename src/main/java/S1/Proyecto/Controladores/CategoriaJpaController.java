5ad65c9418ec6f39315c57b96393c23ef4e1ca73
Wellington <wjose32@hotmail.com>
2019-11-02 12:35:45 -0600

    crear usuario
    se agrega funcionalidad para un inicio de sesion mas rapido

---
+++ src/main/java/S1/Proyecto/Controladores/CategoriaJpaController.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S1.Proyecto.Controladores;
+
+import S1.Proyecto.Controladores.exceptions.NonexistentEntityException;
+import S1.Proyecto.Controladores.exceptions.PreexistingEntityException;
+import S1.Proyecto.Entidades.Categoria;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author WELLINGTON
+ */
+public class CategoriaJpaController implements Serializable {
+
+    public CategoriaJpaController() {
+        this.emf = Persistence.createEntityManagerFactory(NombreConexion.nombreConexion);
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Categoria categoria) throws PreexistingEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(categoria);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            if (findCategoria(categoria.getCodigo()) != null) {
+                throw new PreexistingEntityException("Categoria " + categoria + " already exists.", ex);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Categoria categoria) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            categoria = em.merge(categoria);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                String id = categoria.getCodigo();
+                if (findCategoria(id) == null) {
+                    throw new NonexistentEntityException("The categoria with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(String id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Categoria categoria;
+            try {
+                categoria = em.getReference(Categoria.class, id);
+                categoria.getCodigo();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The categoria with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(categoria);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Categoria> findCategoriaEntities() {
+        return findCategoriaEntities(true, -1, -1);
+    }
+
+    public List<Categoria> findCategoriaEntities(int maxResults, int firstResult) {
+        return findCategoriaEntities(false, maxResults, firstResult);
+    }
+
+    private List<Categoria> findCategoriaEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Categoria.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Categoria findCategoria(String id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Categoria.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getCategoriaCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Categoria> rt = cq.from(Categoria.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
---
+++ src/main/java/S1/Proyecto/Principal/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S1.Proyecto.Principal;
+
+
+import S1.Proyecto.Formularios.Registros.LlamarFormularios;
+/**
+ *
+ * @author WELLINGTON
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LlamarFormularios.llamarLogin();
+    }
+}
